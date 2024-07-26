d462c71ae6b349954d1e065c5191983f5417d932
chneau <charles63500@gmail.com>
2024-07-26 16:16:06 +0100

    chore: Update package version, build configuration, and remove unused code

--- _build.ts
+++ _build.ts
@@ -1,9 +1,14 @@
+import { $ } from "bun";
 import dts from "bun-plugin-dts";
+
+await $`rm -rf dist`;
 
 await Bun.build({
 	entrypoints: ["src/index.ts"],
+	external: ["elysia"],
 	outdir: "dist",
 	target: "node",
+	sourcemap: "linked",
 	minify: true,
 	plugins: [dts()],
 });
